7ff171c6991a11076146cac31f25689fa5563187
Maxim Timofeev <44711150+WaWaKids@users.noreply.github.com>
2020-06-18 00:12:51 +0300

    Update revert_string.c

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,15 @@
 #include "revert_string.h"
-
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 void RevertString(char *str)
 {
-	// your code here
+    int len = strlen(str);
+	char *NewStr = (char*) calloc(len, sizeof(char));
+    int i=0;
+    for(i=len-1;i>-1;i--)
+    {
+    NewStr[len-i-1]= str[i]; 
+    }
+    strcpy(str,NewStr);    
 }
-
